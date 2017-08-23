07ba51d7d9dfdbe63940ed55d2f18c6051a3ad1a
spacexlabs <akhiljalagams@gmail.com>
2017-08-23 21:26:48 +0000

    sync

--- kill.sh
+++
@@ -1,9 +0,0 @@
-set -x
-INTERFACE=wlp0s20u1
-pid=$( create_ap --list-running | grep $INTERFACE | awk '{print $1}' )
-kill $pid
-create_ap --list-running
-
-
-AP_INTERFACE=wlp0s20u1
-echo '0' > /proc/sys/net/ipv4/conf/$AP_INTERFACE/forwarding
